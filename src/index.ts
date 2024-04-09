f95278200e72fe8f2837e08b5a4b178e6674027b
WhiteMind <WhiteMind@qq.com>
2024-04-09 16:44:43 +0800

    fix: frequent timeouts are triggered when recording some Douyin live rooms

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ const checkLiveStatusAndRecord: Recorder['checkLiveStatusAndRecord'] = async fun
   const onEnd = (...args: unknown[]) => {
     this.emit('DebugLog', {
       type: 'common',
-      text: `ffmpeg end, reason: ${JSON.stringify(args)}`,
+      text: `ffmpeg end, reason: ${JSON.stringify(args, (_, v) => (v instanceof Error ? v.stack : v))}`,
     })
     this.recordHandle?.stop()
   }
@@ -111,6 +111,16 @@ const checkLiveStatusAndRecord: Recorder['checkLiveStatusAndRecord'] = async fun
     .inputOptions(
       '-user_agent',
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+      /**
+       * ffmpeg 在处理抖音提供的某些直播间的流时，它会在 avformat_find_stream_info 阶段花费过多时间，这会让录制的过程推迟很久，从而触发超时。
+       * 这里通过降低 avformat_find_stream_info 所需要的字节数量（默认为 5000000）来解决这个问题。
+       *
+       * Refs:
+       * https://github.com/Sunoo/homebridge-camera-ffmpeg/issues/462#issuecomment-617723949
+       * https://stackoverflow.com/a/49273163/21858805
+       */
+      '-probesize',
+      (64 * 1024).toString(),
     )
     .outputOptions(ffmpegOutputOptions)
     .output(recordSavePath)
